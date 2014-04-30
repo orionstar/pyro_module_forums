c92b4c607f850ad3bbd912e67f3771f81c8c8d5c
orionstar <boralaszlo@gmail.com>
2014-04-30 17:27:12 +0200

    Add hungarian localization to the admin interface

---
+++ language/hungarian/forums_lang.php
@@ -0,0 +1,37 @@
+<?php
+
+// Címkék
+$lang['forums_category_label'] 						= 'Kategória';
+$lang['forums_forum_label'] 						= 'Fórum';
+$lang['forums_actions_label'] 						= 'Műveletek';
+$lang['forums_view_label'] 							= 'Megtekint';
+$lang['forums_edit_label'] 							= 'Szerkeszt';
+$lang['forums_delete_label'] 						= 'Töröl';
+$lang['forums_title_label'] 						= 'Cím';
+$lang['forums_description_label'] 					= 'Leírás';
+$lang['forums_no_category_select_label'] 			= '-- Nincs --';
+
+// Címek
+$lang['forums_create_category_title'] 				= 'Kategória hozzáadása';
+$lang['forums_edit_category_title'] 				= '"%s" kategória szerkesztése';
+$lang['forums_category_title'] 						= 'Kategória';
+$lang['forums_list_categories_title'] 				= 'Kategóriák listázása';
+
+$lang['forums_create_forum_title']					= 'Fórum hozzáadása';
+$lang['forums_edit_forum_title']					= '"%s" fórums szerkesztése';
+$lang['forums_forum_title'] 						= 'Fórumok';
+$lang['forums_list_forum_title']					= 'Fórumok listázása';
+
+// Üzenetek
+$lang['forums_no_categories'] 						= 'Nincs kategória hozzáadva.';
+$lang['forums_no_forums']							= 'Nincs fórum hozzáadva.';
+$lang['forums_currently_no_articles'] 				= 'Nincs téma hozzáadva.';
+$lang['forums_category_delete_success'] 			= 'A kategória törölve.';
+$lang['forums_category_add_success'] 				= '"%s" kategória hozzáadva.';
+$lang['forums_category_edit_success'] 				= '"%s" kategória frissítve';
+$lang['forums_category_add_error'] 					= 'Hiba történt.';
+
+$lang['forums_forum_delete_success']				= 'A fórum törölve.';
+$lang['forums_forum_add_success']					= '"%s" fórum hozzáadva.';
+$lang['forums_forum_edit_success']					= '"%s" fórum frissítve.';
+$lang['forums_forum_add_error'] 					= 'Hiba történt.';
